edba2628c62923c04e907affd32005129b7340b8
DevExpressExampleBot <fakeEmail>
2018-06-22 16:24:46 +0300

    Source auto update [skip ci]

---
+++ VB/FindText/MainWindow.xaml.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Windows
+Imports System.Windows.Input
+Imports DevExpress.Xpf.DocumentViewer
+
+Namespace FindText
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+            viewer.OpenDocument("..\..\Demo.pdf")
+        End Sub
+
+        Private Sub viewer_KeyDown(ByVal sender As Object, ByVal e As KeyEventArgs)
+            If e.Key = Key.F3 Then
+                Dim parameters As TextSearchParameter = New TextSearchParameter With { _
+                    .IsCaseSensitive = True, _
+                    .WholeWord = True, _
+                    .Text = "Viewer" _
+                }
+                viewer.FindText(parameters)
+            End If
+        End Sub
+    End Class
+End Namespace
