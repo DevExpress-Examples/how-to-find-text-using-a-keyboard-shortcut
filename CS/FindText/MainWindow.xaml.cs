03b543e6540dc9362ce9b513023b28cd6e5c7523
Alexander Temnov <alexander.temnov@devexpress.com>
2018-06-22 16:21:32 +0300

    add example content

---
+++ CS/FindText/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using DevExpress.Xpf.DocumentViewer;
+
+namespace FindText {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            viewer.OpenDocument(@"..\..\Demo.pdf");
+        }
+
+        private void viewer_KeyDown(object sender, KeyEventArgs e) {
+            if (e.Key == Key.F3) {
+                TextSearchParameter parameters = new TextSearchParameter {
+                    IsCaseSensitive = true,
+                    WholeWord = true,
+                    Text = "Viewer"
+                };
+                viewer.FindText(parameters);
+            }
+        }
+    }
+}
